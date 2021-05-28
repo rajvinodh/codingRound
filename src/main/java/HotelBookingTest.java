28738077e2c6542ad2bbd80ba211f720a1fa0c75
Vinodh Raj R <vinodhrraj@gmail.com>
2021-05-28 11:12:34 +0530

    Update HotelBookingTest.java
    Fixed test to use BaseTest, this test needs to include lot more steps with validations.

--- src/main/java/HotelBookingTest.java
+++ src/main/java/HotelBookingTest.java
@@ -1,4 +1,4 @@
-import com.sun.javafx.PlatformUtil;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -6,48 +6,16 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
-public class HotelBookingTest {
-
-    WebDriver driver = new ChromeDriver();
-
-    @FindBy(linkText = "Hotels")
-    private WebElement hotelLink;
-
-    @FindBy(id = "Tags")
-    private WebElement localityTextBox;
-
-    @FindBy(id = "SearchHotelsButton")
-    private WebElement searchButton;
-
-    @FindBy(id = "travellersOnhome")
-    private WebElement travellerSelection;
+public class HotelBookingTest extends BaseTest {
 
     @Test
     public void shouldBeAbleToSearchForHotels() {
-        setDriverPath();
+        driver.findElement(By.linkText("Hotels")).click();
 
-        driver.get("https://www.cleartrip.com/");
-        hotelLink.click();
+        driver.findElement(By.id("Tags")).sendKeys("Indiranagar, Bangalore");
 
-        localityTextBox.sendKeys("Indiranagar, Bangalore");
-
-        new Select(travellerSelection).selectByVisibleText("1 room, 2 adults");
-        searchButton.click();
-
-        driver.quit();
-
-    }
-
-    private void setDriverPath() {
-        if (PlatformUtil.isMac()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver");
-        }
-        if (PlatformUtil.isWindows()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        }
-        if (PlatformUtil.isLinux()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
-        }
+        new Select(driver.findElement(By.id("travellersOnhome"))).selectByVisibleText("2 rooms, 4 adults");
+        driver.findElement(By.id("SearchHotelsButton")).click();
     }
 
 }
