7cb46e17486430ec2b1b9ebbd99344aadc599a0b
Vinodh Raj R <vinodhrraj@gmail.com>
2021-05-28 11:08:31 +0530

    Update FlightBookingTest.java
    Fixed "ToTag" id.
    Included logic to select a date randomly.
    And made changes related to BaseTest.

--- src/main/java/FlightBookingTest.java
+++ src/main/java/FlightBookingTest.java
@@ -1,4 +1,5 @@
 import com.sun.javafx.PlatformUtil;
+import com.sun.xml.internal.rngom.parse.host.Base;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
@@ -9,60 +10,41 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.List;
+import java.util.Random;
 
-public class FlightBookingTest {
-
-    WebDriver driver = new ChromeDriver();
-
+public class FlightBookingTest extends BaseTest {
 
     @Test
     public void testThatResultsAppearForAOneWayJourney() {
-
-        setDriverPath();
-        driver.get("https://www.cleartrip.com/");
-        waitFor(2000);
         driver.findElement(By.id("OneWay")).click();
 
         driver.findElement(By.id("FromTag")).clear();
         driver.findElement(By.id("FromTag")).sendKeys("Bangalore");
 
-        //wait for the auto complete options to appear for the origin
-
-        waitFor(2000);
         List<WebElement> originOptions = driver.findElement(By.id("ui-id-1")).findElements(By.tagName("li"));
         originOptions.get(0).click();
 
-        driver.findElement(By.id("toTag")).clear();
-        driver.findElement(By.id("toTag")).sendKeys("Delhi");
+        driver.findElement(By.id("ToTag")).clear();
+        driver.findElement(By.id("ToTag")).sendKeys("Delhi");
 
         //wait for the auto complete options to appear for the destination
-
-        waitFor(2000);
         //select the first item from the destination auto complete list
         List<WebElement> destinationOptions = driver.findElement(By.id("ui-id-2")).findElements(By.tagName("li"));
         destinationOptions.get(0).click();
 
-        driver.findElement(By.xpath("//*[@id='ui-datepicker-div']/div[1]/table/tbody/tr[3]/td[7]/a")).click();
+        List<WebElement> selectableDate = driver.findElements(By.xpath("//a[@class=\"ui-state-default \"]"));
+        WebElement randomDate = selectableDate.get(new Random().nextInt(selectableDate.size()));
+        String dateSelected = randomDate.getText();
+        randomDate.click();
+        System.out.println("Selected Date: " + dateSelected);
 
         //all fields filled in. Now click on search
         driver.findElement(By.id("SearchBtn")).click();
 
-        waitFor(5000);
         //verify that result appears for the provided journey search
-        Assert.assertTrue(isElementPresent(By.className("searchSummary")));
+        softAssert.assertTrue(isElementPresent(By.className("searchSummary")));
 
-        //close the browser
-        driver.quit();
-
-    }
-
-
-    private void waitFor(int durationInMilliSeconds) {
-        try {
-            Thread.sleep(durationInMilliSeconds);
-        } catch (InterruptedException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
+        softAssert.assertAll();
     }
 
 
@@ -72,18 +54,6 @@ public class FlightBookingTest {
             return true;
         } catch (NoSuchElementException e) {
             return false;
-        }
-    }
-
-    private void setDriverPath() {
-        if (PlatformUtil.isMac()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver");
-        }
-        if (PlatformUtil.isWindows()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        }
-        if (PlatformUtil.isLinux()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
         }
     }
 }
